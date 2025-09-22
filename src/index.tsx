27275496554fab9a3aaec1dcbdaf1bea5d5fa78f
zispidd <zispidd@gmail.com>
2025-09-22 19:23:51 +0200

    chore: add export function

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,5 @@ import { NitroModules } from 'react-native-nitro-modules'
 import type { NitroUserAgent } from './specs/NitroUserAgent.nitro'
 
 export const nitroUserAgent = NitroModules.createHybridObject<NitroUserAgent>('NitroUserAgent')
+
+export const getUserAgent = nitroUserAgent.getUserAgent
